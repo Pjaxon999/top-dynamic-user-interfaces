2af83a87f52635d0af5cba862f5e42efe0f775da
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-04-03 11:22:26 -0600

    Add carousel functionality, add images to dist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "./styles/modern-normalize.css";
 import "./styles/styles.css";
 import dropdown from "./modules/dropdown";
+import carousel from "./modules/carousel";
 
 
-// add dropdown functionality
+// add dropdown functionality by grabbing the button and attaching the event listener
 const menuButton = document.querySelector("nav > button");
 menuButton.addEventListener("click", dropdown);
+
+// add carousel functionality
+carousel();
---
+++ src/modules/carousel.js
@@ -0,0 +1,75 @@
+export default function carousel() {
+    // Initialize array to store indices
+    const imgArray = [];
+
+    // store the current data-index (default/initial value is 0) and initialize newIndex for later use
+    let currentIndex = 0;
+    let newIndex;
+
+    // Populate array based on data indices of images
+    function getImageData (){
+        const carouselImageData = document.querySelectorAll("[data-index]");
+        carouselImageData.forEach(image => {
+        imgArray.push(Number(image.dataset.index));
+        });
+    }
+
+
+    // Render function. Grab the current image, toggle it's class, update the index, grab the new image, toggle again
+    function renderCarousel(newIndex){
+        const currentImage = document.querySelector(`[data-index = "${currentIndex}"]`);
+        currentImage.classList.toggle("hidden");
+        currentIndex = newIndex;
+        const newImage = document.querySelector(`[data-index="${currentIndex}"]`);
+        newImage.classList.toggle("hidden");
+    }
+    
+
+    // In order to render, we need to generate a new index value based on the state of the carousel
+    // Left arrow functionality. If the index is 0, we go to the highest index image
+    function leftArrowHandler() {
+        if (currentIndex !== 0){
+            newIndex = currentIndex - 1;
+            renderCarousel(newIndex);
+        } else {
+            newIndex = imgArray.length - 1;
+            renderCarousel(newIndex)
+        }
+    }
+
+    // Right arrow functionality. If it is the last image in the carousel (array length - 1), we go to the first image
+    function rightArrowHandler() {
+        if (currentIndex !== imgArray.length - 1){
+            newIndex = currentIndex + 1;
+            renderCarousel(newIndex);
+        } else {
+            newIndex = 0;
+            renderCarousel(newIndex);
+        }
+    }
+
+    // Image nav dot functionality. New index is set to the corresponding button's index. Only fires if a new index is clicked.
+    function clickHandler(e) {
+        const clickedIndex = Number(e.target.dataset.action);
+        if (clickedIndex !== currentIndex){
+            newIndex = clickedIndex;
+            renderCarousel(newIndex);
+        } else {
+            return
+        }
+    }
+
+    // add carousel functionality by grabbing all the relevant buttons and attaching event listeners
+    function attachEventListeners() {
+        const leftArrow = document.querySelector("[data-action = 'left']");
+        const rightArrow = document.querySelector("[data-action = 'right']");
+        const imageNavDots = document.querySelectorAll(".img-nav > button");
+
+        leftArrow.addEventListener("click", leftArrowHandler);
+        rightArrow.addEventListener("click", rightArrowHandler);
+        imageNavDots.forEach((button) => button.addEventListener("click", clickHandler));
+    }
+    getImageData();
+    attachEventListeners();
+
+}
